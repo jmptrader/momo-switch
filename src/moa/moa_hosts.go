d1c9885ac80ec0a17804765140e96f4802917286
blackbeans <blackbeans.zc@gmail.com>
2014-01-16 20:43:52 +0800

    commit

--- src/moa/moa_hosts.go
+++ src/moa/moa_hosts.go
@@ -110,7 +110,7 @@ func (self *MoaInStanceManager) syncMoaHosts() {
 					})
 
 					if len(instance.Name) <= 0 {
-						continue
+						return
 					}
 
 					instance.RestartUrl = baseUrl + "/index.html?processname=" + instance.Name + "&amp;action=restart"
