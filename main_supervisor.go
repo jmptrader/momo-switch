1004d2086c79555eb87db6e3d5bb7e0a3e3ce6d9
blackbeans <blackbeans.zc@gmail.com>
2014-12-09 17:07:00 +0800

    modified:   main_supervisor.go

--- main_supervisor.go
+++ main_supervisor.go
@@ -19,17 +19,36 @@ type SupervisorInstance struct {
 }
 
 func main() {
-	hosts := []string{"task001.m6",
-		"task002.m6",
-		"task003.m6",
-		"task004.m6",
-		"task005.m6",
-		"task006.m6",
-		"task007.m6",
-		"task008.m6",
-		"task009.m6",
-		"task010.m6",
+
+	hosts := []string{
+		"xxx001.m6",
+		"xxx002.m6",
+		"xxx003.m6",
+		"xxx004.m6",
+		"xxx005.m6",
+		"xxx006.m6",
+		"xxx007.m6",
+		"xxx008.m6",
+		"xxx009.m6",
+		"xxx010.m6",
+		"xxx011.m6",
+		"xxx012.m6",
+		"xxx013.m6",
+		"xxx014.m6",
+		"xxx015.m6",
+		"xxx016.m6",
+		"xxx017.m6",
+		"xxx018.m6",
+		"xxx019.m6",
+		"xxx020.m6",
+		"xxx021.m6",
+		"xxx022.m6",
+		"xxx023.m6",
+		"xxx024.m6",
+		"xxx025.m6",
+		"xxx026.m6",
 	}
+
 	store := make(map[string][]string, 20)
 	for _, v := range hosts {
 		baseUrl := "http://" + v + ":9001"
@@ -53,6 +72,10 @@ func main() {
 
 				})
 
+				if instance.Status != "running" || instance.Name == "flume-agent" {
+					return
+				}
+
 				arr, ok := store[instance.Name]
 				if !ok {
 					arr = make([]string, 0, 10)
@@ -65,8 +88,12 @@ func main() {
 		}
 	}
 
+	table := "||||\n" +
+		"|:--:|:--:|----|\n" +
+		"| 服务名称|数量| 部署机器 |\n"
+
 	for k, v := range store {
-		label := k + "\t\t\t"
+		label := ""
 		for i, host := range v {
 			label += host
 			if i < len(v)-1 {
@@ -74,7 +101,9 @@ func main() {
 			}
 		}
 
-		fmt.Printf("* \t%s\n", label)
+		table += fmt.Sprintf("|%s|%d|%s|\n", k, len(v), label)
 	}
+
+	fmt.Println(table)
 
 }
